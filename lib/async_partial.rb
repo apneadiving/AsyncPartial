684e23509c3f933400943d91f8249b789132efab
David Ruyer <druyer@hypios.com>
2011-05-10 23:14:24 +0200

    Application helpers initialized explicitly
    added a yield in the content_for :scripts

--- lib/async_partial.rb
+++ lib/async_partial.rb
@@ -1,7 +1,10 @@
-require 'application_helper'
+require 'async_partial/application_helper'
 require 'base/base'
 
 module RenderAsync
   class Engine < Rails::Engine
+    initializer 'async_partial.helper' do |app|
+      ActionView::Base.send :include, AsyncPartial::ApplicationHelper
+    end
   end
 end
--- lib/async_partial/application_helper.rb
+++ lib/async_partial/application_helper.rb
@@ -1,4 +1,4 @@
-module ApplicationHelper
+module AsyncPartial::ApplicationHelper
   def async_partial *args
     vars = AsyncPartial.create_url_and_random(args, params[:controller])
     render :partial => "/async_partial/base", :locals => {:url => vars[:url], :random => vars[:random]}
